1075097825d15304fdd3c672314f0da9c6ee09c8
Paulina Borym <paulina.borzym@op.pl>
2023-07-05 15:59:04 +0200

    Add controller

--- src/main/java/com/project/controller/GameController.java
+++ src/main/java/com/project/controller/GameController.java
@@ -1,4 +1,52 @@
 package com.project.controller;
 
+import com.project.domain.Game;
+import com.project.dto.GameDto;
+import com.project.exeption.GameNotFoundException;
+import com.project.mapper.GameMapper;
+import com.project.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/games")
 public class GameController {
+    private final GameService service;
+    private final GameMapper gameMapper;
+
+    @GetMapping
+    public ResponseEntity<List<GameDto>> getGames(){
+        List<Game> games = service.getAllGames();
+        return ResponseEntity.ok(gameMapper.mapToGameDtoList(games));
+    }
+
+    @GetMapping(value = "{gameId}")
+    public ResponseEntity<GameDto> getGame(@PathVariable Long gameId) throws GameNotFoundException {
+        return ResponseEntity.ok(gameMapper.mapToGameDto(service.getGame(gameId)));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void>createGame(@RequestBody GameDto gameDto) {
+        Game game = gameMapper.mapToGame(gameDto);
+        service.saveGame(game);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<GameDto> updateGame(@RequestBody GameDto gameDto) {
+        Game game = gameMapper.mapToGame(gameDto);
+        Game savedGame = service.saveGame(game);
+        return ResponseEntity.ok(gameMapper.mapToGameDto(savedGame));
+    }
+
+    @DeleteMapping(value = "{gameId}")
+    public ResponseEntity<Void> deleteGame(@PathVariable Long gameId) throws GameNotFoundException {
+        service.deleteGame(gameId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.project.controller;
 
+import com.project.domain.User;
+import com.project.dto.UserDto;
+import com.project.exeption.UserNotFoundException;
+import com.project.mapper.UserMapper;
+import com.project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/users")
 public class UserController {
+
+    private final UserService service;
+    private final UserMapper userMapper;
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>>getUsers(){
+        List<User> users = service.getAllUsers();
+        return ResponseEntity.ok(userMapper.mapToUserDtoList(users));
+    }
+
+    @GetMapping(value = "{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) throws UserNotFoundException {
+       return ResponseEntity.ok(userMapper.mapToUserDto(service.getUser(userId)));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void>createUser(@RequestBody UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
+        service.saveUser(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
+        User user = userMapper.mapToUser(userDto);
+        User savedUser = service.saveUser(user);
+        return ResponseEntity.ok(userMapper.mapToUserDto(savedUser));
+    }
+
+    @DeleteMapping(value = "{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) throws UserNotFoundException {
+        service.deleteUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
 }
